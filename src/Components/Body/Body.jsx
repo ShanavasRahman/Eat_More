6f4343f6c4316aad02596fbd7a478ff9bac80a5a
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-11-25 12:26:59 +0530

    filtering on the basis of rating

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ResCard from "./ResCard";
 import ResList from "../Utils/resList";
 
 const Body = () => {
+  const [res, setRes] = useState(ResList);
+  function filterRes() {
+    console.log("hello")
+    const filteredRes = res.filter((restuarant) => restuarant.avgRating >= 4.5);
+    setRes(filteredRes);
+  }
   return (
     <div>
       <div className='pl-8 pt-8'>
-        <button className='bg-neutral-500 px-7 py-2 rounded-full text-neutral-50'>
+        <button className='bg-blue-gray-200 px-7 py-2 rounded-full text-blue-gray-900' onClick={filterRes}>
           Top rated restuarant
         </button>
       </div>
       <div className='flex flex-wrap p-8 gap-10'>
-        {ResList.map((restuarant) => (
+        {res.map((restuarant) => (
           <ResCard props={restuarant} key={restuarant.id} />
         ))}
       </div>
