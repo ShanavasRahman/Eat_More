f7261844b607b100f992c6ffcab439a06a41664a
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-11-25 17:15:49 +0530

    created my own git api

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,38 +1,53 @@
 import React, { useEffect, useState } from "react";
 import ResCard from "./ResCard";
-import ResList from "../Utils/resList";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [res, setRes] = useState([]);
+
   useEffect(() => {
     fetchData();
-  }, [])
-  
+  }, []);
 
   async function fetchData() {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-    const jsonData = data.json();
-    console.log(jsonData);
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/ShanavasRahman/data/refs/heads/master/data.json"
+      );
+      if (!response.ok) {
+        throw new Error(`Failed to fetch. Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setRes(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
+
   function filterRes() {
-    const filteredRes = res.filter((restuarant) => restuarant.avgRating >= 4.5);
+    const filteredRes = res.filter((restuarant) => restuarant.avgRating > 4.3);
     setRes(filteredRes);
   }
+
+
+
   if (res.length === 0) {
     return (
-      <div className="flex flex-wrap p-8 gap-10 justify-center">
+      <div className='flex flex-wrap p-8 gap-10 justify-center'>
         {Array.from({ length: 8 }).map((_, index) => (
-          <Shimmer key={index}/>
+          <Shimmer key={index} />
         ))}
-        
       </div>
-    )
+    );
   }
+
+
   return (
     <div>
       <div className='pl-8 pt-8'>
-        <button className='bg-blue-gray-200 px-7 py-2 rounded-full text-blue-gray-900' onClick={filterRes}>
+        <button
+          className='bg-blue-gray-200 px-7 py-2 rounded-full text-blue-gray-900'
+          onClick={filterRes}>
           Top rated restuarant
         </button>
       </div>
