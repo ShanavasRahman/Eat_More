894f8d5d1c089b61afcbd8fb2e08e75d6ed55f19
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-12-02 15:58:38 +0530

    style modifying

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -6,11 +6,14 @@ import useFetchData from "../../Utils/useFetch";
 import resDetails from "../useContextApi";
 
 const Body = () => {
-
   //Using context api
-  const { filteredRestuarant, setFilteredRestuarant, restuarants, setRestuarants } =
-    useContext(resDetails);
-  
+  const {
+    filteredRestuarant,
+    setFilteredRestuarant,
+    restuarants,
+    setRestuarants,
+  } = useContext(resDetails);
+
   const data = useFetchData();
 
   useEffect(() => {
@@ -21,10 +24,12 @@ const Body = () => {
   }, [data]);
 
   function filterRes() {
-    const filteredRes = restuarants.filter((restuarant) => restuarant.avgRating > 4.3);
+    const filteredRes = restuarants.filter(
+      (restuarant) => restuarant.avgRating > 4.3
+    );
     setFilteredRestuarant(filteredRes);
   }
-  
+
   //Higher Order Component
   const PromotedResCard = resCardWithPromoted(ResCard);
 
@@ -39,11 +44,12 @@ const Body = () => {
       {/* Filter Button Section */}
       <div className='px-4 md:px-12 pt-4 md:pt-8 flex justify-center md:justify-start'>
         <button
-          className='bg-blue-gray-200 px-6 md:px-7 py-2 rounded-full text-blue-gray-900 text-sm md:text-base shadow-md hover:bg-blue-gray-300 transition-colors' onClick={filterRes}>
-          Top rated restaurant
+          className='bg-gradient-to-r from-blue-500 to-blue-600 px-6 md:px-7 py-2 rounded-full text-white text-sm md:text-base font-semibold shadow-md hover:from-blue-600 hover:to-blue-500 transform hover:scale-105 transition-all duration-300 ease-in-out'
+          onClick={filterRes}>
+          Top Rated Restaurant
         </button>
       </div>
-  
+
       {/* Restaurants Display Section */}
       <div className='flex flex-wrap p-4 md:p-8 gap-6 md:gap-10 justify-center'>
         {filteredRestuarant.map((restaurant) => (
@@ -58,7 +64,6 @@ const Body = () => {
       </div>
     </div>
   );
-  
 };
 
 export default Body;
